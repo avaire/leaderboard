460bcd9ec2cf86f5097c4543e3ea5979258c0c7d
Alexis Tan <alexis@sen-dev.com>
2019-02-27 15:13:52 +0100

    Change the leaderboard table layout to look a bit nicer

--- assets/js/views/PlayerExperienceTableRow.vue
+++ assets/js/views/PlayerExperienceTableRow.vue
@@ -7,8 +7,10 @@
             </figure>
         </td>
         <td class="username">
-            <strong>{{ user.username }}</strong>
-            <span class="discriminator">#{{ user.discriminator }}</span>
+            <span>
+                <strong>{{ user.username }}</strong>
+                <span class="discriminator">#{{ user.discriminator }}</span>
+            </span>
         </td>
         <td class="xp-bar has-text-centered">
             <div class="xp"><strong>{{ xpInCurrentLevel }}</strong> out of <strong>{{ totalXPForLevel }}</strong> XP (Total: <strong>{{ formatNumber(user.experience - 100) }}</strong>)</div>
@@ -21,6 +23,49 @@
         </td>
     </tr>
 </template>
+
+<style type="text/css" scoped>
+    td {
+        padding: 0;
+    }
+    td.rank {
+        font-size: 1.5rem;
+        width: 5%;
+    }
+    td.avatar {
+        width: 5%;
+    }
+    td.avatar figure {
+        margin-left: 0px;
+        margin-right: 0px;
+    }
+    td.username {
+        font-size: 1.15rem;
+        width: 25%;
+    }
+    td.username > span {
+        vertical-align: -webkit-baseline-middle;
+    }
+    td.username span.discriminator {
+        font-size: 0.95rem;
+    }
+    td.xp-bar {
+        width: 55%;
+    }
+    td.level {
+        vertical-align: -webkit-baseline-middle;
+        width: 10%;
+    }
+
+    @media only screen and (max-width: 820px) {
+        td.xp-bar {
+            display: none;
+        }
+        td.username {
+            width: 80%;
+        }
+    }
+</style>
 
 <script>
     import {
